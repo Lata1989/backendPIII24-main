062857cf311d9705ab4c61f0bff9cba4626c824a
Pablo Alejandro de la Iglesia <143137265+Lata1989@users.noreply.github.com>
2024-06-24 19:02:12 -0300

    modificacion del puerto, el swagger ahora anda

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS
 app.use(cors());
